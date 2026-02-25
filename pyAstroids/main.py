7178bda6be02e9c414e5abeffd4b48d774d9d21a
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-02-25 14:41:53 +0100

    11: added groups in main for cleaner code

--- pyAstroids/main.py
+++ pyAstroids/main.py
@@ -10,9 +10,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+
     player_x = (SCREEN_WIDTH / 2)
     player_y = (SCREEN_HEIGHT / 2)
     player = Player(player_x, player_y)
+
+
 
     while True:
         log_state()
@@ -20,10 +28,11 @@ def main():
             if event.type == pygame.QUIT:
                 return      
         
-        player.update(dt) 
+        updatable.update(dt) 
 
         screen.fill("black")
-        player.draw(screen)
+        for game_object in drawable:
+            game_object.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
