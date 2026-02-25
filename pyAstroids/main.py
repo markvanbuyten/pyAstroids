a02b2ac8e9560daa1e6ce7831e14e228e6acb54d
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-02-25 15:27:28 +0100

    13: added collitions

--- pyAstroids/main.py
+++ pyAstroids/main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -37,6 +38,12 @@ def main():
                 return      
         
         updatable.update(dt) 
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print(f"Game over!")
+                sys.exit()
 
         screen.fill("black")
         for game_object in drawable:
