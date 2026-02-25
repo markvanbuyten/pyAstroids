e4d44c8eab05d2012ae18bb30379294016888538
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-02-25 14:25:11 +0100

    9: added left and right movement for player

--- pyAstroids/main.py
+++ pyAstroids/main.py
@@ -18,13 +18,17 @@ def main():
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
+                return      
+        
+        player.update(dt) 
 
+        screen.fill("black")
         player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+         
 
         
         
