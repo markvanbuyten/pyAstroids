50c451062952e8863ec7e02c917bcea51e31baa5
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-02-25 14:08:58 +0100

    8: created a player class, added it to main and draw theplayer to the screen

--- pyAstroids/main.py
+++ pyAstroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,14 +10,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player_x = (SCREEN_WIDTH / 2)
+    player_y = (SCREEN_HEIGHT / 2)
+    player = Player(player_x, player_y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+        
         
 
 
