050129f166d007b9d5455f7560b618c42440ef8d
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-02-25 13:25:44 +0100

    6: added FPS to the game loop

--- pyAstroids/main.py
+++ pyAstroids/main.py
@@ -5,6 +5,10 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -12,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
